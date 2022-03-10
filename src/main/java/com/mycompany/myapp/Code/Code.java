9ca7bbb9af7fc15fd9ba79054e83fa9f562d2962
user <user@14.53.248.228>
2022-03-11 00:07:42 +0900

    dddddd

--- src/main/java/com/mycompany/myapp/Code/Code.java
+++ src/main/java/com/mycompany/myapp/Code/Code.java
@@ -6,24 +6,50 @@ public class Code {
 	private String IfcgName;
 	private String IfcgDelNy;
 	
+	private String ifcdSeq;
+	private String ifcdName;
+	private String ifcdDelNy;
+	//private String ifcgSeq;
 	
 	
 	public String getIfcgSeq() {
 		return IfcgSeq;
 	}
-	public void setIfcgSeq(String IfcgSeq) {
-		this.IfcgSeq = IfcgSeq;
+	public void setIfcgSeq(String ifcgSeq) {
+		IfcgSeq = ifcgSeq;
 	}
 	public String getIfcgName() {
 		return IfcgName;
 	}
-	public void setIfcgName(String IfcgName) {
-		this.IfcgName = IfcgName;
+	public void setIfcgName(String ifcgName) {
+		IfcgName = ifcgName;
 	}
 	public String getIfcgDelNy() {
 		return IfcgDelNy;
 	}
-	public void setIfcgDelNye(String IfcgDelNy) {
-		this.IfcgDelNy = IfcgDelNy;
+	public void setIfcgDelNy(String ifcgDelNy) {
+		IfcgDelNy = ifcgDelNy;
 	}
+	public String getIfcdSeq() {
+		return ifcdSeq;
+	}
+	public void setIfcdSeq(String ifcdSeq) {
+		this.ifcdSeq = ifcdSeq;
+	}
+	public String getIfcdName() {
+		return ifcdName;
+	}
+	public void setIfcdName(String ifcdName) {
+		this.ifcdName = ifcdName;
+	}
+	public String getIfcdDelNy() {
+		return ifcdDelNy;
+	}
+	public void setIfcdDelNy(String ifcdDelNy) {
+		this.ifcdDelNy = ifcdDelNy;
+	}
+	
+	
+
+	
 }
