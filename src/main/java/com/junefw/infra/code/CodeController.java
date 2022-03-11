f3eecd1ca98bc876ab71054705c0b887ab01c09d
user <user@14.53.248.228>
2022-03-11 11:10:00 +0900

    dddddddddddddddd

--- src/main/java/com/junefw/infra/code/CodeController.java
+++ src/main/java/com/junefw/infra/code/CodeController.java
@@ -1,5 +1,60 @@
 package com.junefw.infra.code;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CodeController {
 
+	@Autowired
+	CodeServiceImpl service;
+	
+	@RequestMapping(value = "/code/codeList")
+	public String CodeGroupList(Model model) throws Exception {
+
+		List<Code> list = service.selectListCode();
+		
+		model.addAttribute("list", list);
+
+		return "code/CodeGroupList";
+	}
+	
+	@RequestMapping(value = "/code/codeGroupForm")
+	public String codeGroupForm(Model model) throws Exception {
+
+		return "code/codeGroupForm";
+	}
+	
+	@RequestMapping(value = "/code/codeGroupInst")
+	public String codeGroupInst(Code dto) throws Exception {
+		
+		System.out.println();
+		
+		service.insert(dto);
+		
+		return "";
+	}
+	@RequestMapping(value = "/code/codeGroupInst")
+	public String codeView(CodeVo vo, Model model) throws Exception {
+		
+		Code rt= service.selectOne(vo);
+		
+		model.addAttribute("item", rt);
+		
+		return "";
+	}
+	
+	@RequestMapping(value = "/code/codeGroupForm2")
+	public String codeGroupForm2(CodeVo vo, Model model) throws Exception {
+		
+		Code rt= service.selectOne(vo);
+		
+		model.addAttribute("item", rt);
+		
+		return "code/codeGroupForm2";
+	}
 }
--- src/main/java/com/junefw/infra/code/CodeServiceImpl.java
+++ src/main/java/com/junefw/infra/code/CodeServiceImpl.java
@@ -1,7 +1,64 @@
 package com.junefw.infra.code;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.junefw.infra.member.Member;
+
 public class CodeServiceImpl implements CodeService{
 	
+	@Autowired
+	CodeDao dao;
+
+	@Override
+	public List<Code> selectList() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insert(Code dao) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Code selectOne(CodeVo vo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Code dto) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Code> selectListCode() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int insertCode(Code dao) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Code selectOneCode(CodeVo vo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int updateCode(Code dto) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 	
+
 
 }
